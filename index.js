c1b29db55ac7843494c339f30d0a6b8c641bb8c3
josesangeeth <josesangeeth14@gmail.com>
2021-09-18 20:32:54 +0530

    callback hell countdown commited

---
+++ index.js
@@ -0,0 +1,45 @@
+let count = 10;
+setTimeout(()=>{
+    const content = document.querySelector('#countdown');
+    content.innerHTML = count--;
+    setTimeout(()=>{
+        const content = document.querySelector('#countdown');
+        content.innerHTML = count--;
+        setTimeout(()=>{
+            const content = document.querySelector('#countdown');
+            content.innerHTML = count--;
+            setTimeout(()=>{
+                const content = document.querySelector('#countdown');
+                content.innerHTML = count--;
+                setTimeout(()=>{
+                    const content = document.querySelector('#countdown');
+                    content.innerHTML = count--;
+                    setTimeout(()=>{
+                        const content = document.querySelector('#countdown');
+                        content.innerHTML = count--;
+                        setTimeout(()=>{
+                            const content = document.querySelector('#countdown');
+                            content.innerHTML = count--;
+                            setTimeout(()=>{
+                                const content = document.querySelector('#countdown');
+                                content.innerHTML = count--;
+                                setTimeout(()=>{
+                                    const content = document.querySelector('#countdown');
+                                    content.innerHTML = count--;
+                                    setTimeout(()=>{
+                                        const content = document.querySelector('#countdown');
+                                        content.innerHTML = count--;
+                                        setTimeout(()=>{
+                                            const content = document.querySelector('#countdown');
+                                            content.innerHTML = "Happy Independence Day";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
